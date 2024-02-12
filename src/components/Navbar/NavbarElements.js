e567db6732a2e264d85e192228cd1712ade4ff9f
LeeviMakinen <leevi.kalle.makinen@gmail.com>
2024-02-12 15:43:04 +0200

    Demoilua ja populointiskripti

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,5 +1,6 @@
 // Filename - ./components/Navbar.js
 
+// Kirjoita käsin alempi rivi, niin ide tarjoaa importtia
 import {FaBars } from "react-icons/fa"
 import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
