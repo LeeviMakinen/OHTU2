bff3f744eaed9112d415e29e2d20d350bb65aa60
Topi Purtola <tpurtola@uef.fi>
2024-03-06 12:14:56 +0200

    Moose Of Dead: Pisteenlaskun aloitus,useEffectin lisääminen, kehittäjänumeroiden käyttäminen

--- src/components/Pages/MooseOfDead/Pelisali.js
+++ src/components/Pages/MooseOfDead/Pelisali.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Slot1 from "./Kuvat/Slot1.png"
 import Slot2 from "./Kuvat/Slot2.png"
 import Slot3 from "./Kuvat/Slot3.png"
@@ -10,27 +10,43 @@ import BigWin1 from "./Kuvat/BigWin1.png"
 import BigWin2 from "./Kuvat/BigWin2.png"
 import BigWin3 from "./Kuvat/BigWin3.png"
 import Scatter from "./Kuvat/SCATTER.png"
+import Theme from "./Äänet/theme.wav"
+import Lataus from "./Äänet/Lataus.mp3"
+import Pyssy from "./Äänet/Pyssy.mp3"
 
 
 
 const Toiminnallisuus = () => {
 
-    const slot1 = <img src={Slot1} alt={"slot1"} width={120} height={120}/>;
-    const slot2 = <img src={Slot2} alt={"slot2"} width={120} height={120}/>;
-    const slot3 = <img src={Slot3} alt={"slot3"} width={120} height={120}/>;
-    const slot4 = <img src={Slot4} alt={"slot4"} width={120} height={120}/>;
-    const slot5 = <img src={Slot5} alt={"slot5"} width={120} height={120}/>;
-    const slot6 = <img src={Slot6} alt={"slot6"} width={120} height={120}/>;
-    const slot7 = <img src={Slot7} alt={"slot7"} width={120} height={120}/>;
-    const bigwin1 = <img src={BigWin1} alt={"bigwin1"} width={120} height={120}/>;
-    const bigwin2 = <img src={BigWin2} alt={"bigwin2"} width={120} height={120}/>;
-    const bigwin3 = <img src={BigWin3} alt={"bigwin3"} width={120} height={120}/>;
-    const scatter = <img src={Scatter} alt={"bigwin3"} width={120} height={120}/>;
+    //Äänet
+    const lataus= new Audio(Lataus)
+    const theme = new Audio(Theme)
+    const pyssy = new Audio(Pyssy)
 
+    //Slotit
+    const slot1 = <img src={Slot1} alt={"slot1"} width={120} height={120} />;
+    const slot2 = <img src={Slot2} alt={"slot2"} width={120} height={120} />;
+    const slot3 = <img src={Slot3} alt={"slot3"} width={120} height={120} />;
+    const slot4 = <img src={Slot4} alt={"slot4"} width={120} height={120} />;
+    const slot5 = <img src={Slot5} alt={"slot5"} width={120} height={120} />;
+    const slot6 = <img src={Slot6} alt={"slot6"} width={120} height={120} />;
+    const slot7 = <img src={Slot7} alt={"slot7"} width={120} height={120} />;
+    const bigwin1 = <img src={BigWin1} alt={"bigwin1"} width={120} height={120} />;
+    const bigwin2 = <img src={BigWin2} alt={"bigwin2"} width={120} height={120} />;
+    const bigwin3 = <img src={BigWin3} alt={"bigwin3"} width={120} height={120} />;
+    const scatter = <img src={Scatter} alt={"bigwin3"} width={120} height={120} />;
+
+    //Arrayt slottiriveille
     const [arvot1,setArvot1] = useState([slot1,slot1,slot1,slot1,slot1])
     const [arvot2,setArvot2] = useState([slot1,slot1,slot1,slot1,slot1])
     const [arvot3,setArvot3] = useState([slot1,slot1,slot1,slot1,slot1])
 
+    //Pisteet
+    const [pisteet,setPisteet] = useState(0.0)
+
+    useEffect(() => {
+        pisteenlasku(arvot1);
+    }, [arvot1]);
 
     function randomit(min,max) {
 
@@ -38,6 +54,8 @@ const Toiminnallisuus = () => {
         const maximi = Math.floor(max);
         const randomluku = Math.floor(Math.random() * (maximi - minimi + 1) + minimi);
 
+        return randomluku;
+        /*
         if (randomluku === 1) {
             return slot1;
         }
@@ -71,17 +89,28 @@ const Toiminnallisuus = () => {
         else if (randomluku === 11) {
             return scatter;
         }
-
+*/
 
     }
 
 
     function spinni() {
-        setArvot1([randomit(1, 11),randomit(1,11),randomit(1,10),randomit(1,11),randomit(1,11)])
-        setArvot2([randomit(1, 11),randomit(1,11),randomit(1,10),randomit(1,11),randomit(1,11)])
-        setArvot3([randomit(1, 11),randomit(1,10),randomit(1,11),randomit(1,11),randomit(1,11)])
+        lataus.play()
+        const newArvot1=([randomit(1, 11),randomit(1,11),randomit(1,10),randomit(1,11),randomit(1,11)])
+        const newArvot2=([randomit(1, 11),randomit(1,11),randomit(1,10),randomit(1,11),randomit(1,11)])
+        const newArvot3 =([randomit(1, 11),randomit(1,10),randomit(1,11),randomit(1,11),randomit(1,11)])
+        setArvot1(newArvot1)
+        setArvot2(newArvot2)
+        setArvot3(newArvot3)
+        console.log(newArvot1)
     }
 
+
+    function pisteenlasku(voittorivi){
+        if (voittorivi[0] === 1){
+            setPisteet(pisteet+1)
+        }
+    }
 
     return(
         <div>
@@ -92,6 +121,7 @@ const Toiminnallisuus = () => {
             </ul>
 
             <button onClick={spinni}> SPIN AND WIN</button>
+            Voitot: {pisteet} €
         </div>
 
     )
