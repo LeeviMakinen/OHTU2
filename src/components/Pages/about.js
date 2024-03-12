a21ac9b43ca73e5da3a85fb75d9d2dd18b5422e0
JimiSar <jimisar@uef.fi>
2024-03-13 00:38:14 +0200

    Jimikin onnistui :DDDDD

--- src/components/Pages/about.js
+++ src/components/Pages/about.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./pages.css";
-import kuva from "./pojat.PNG";
+import "./about.css";
+import hirvenpyytaja from "./hirvenpyytäjä1.png"
 const About = () => {
     return (
 
@@ -8,33 +8,124 @@ const About = () => {
             <div className="container">
                 <div className="post">
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div class="col-lg-12">
                             <div id="Kuka">
-                                <p>Jimi ja Leevi tapasivat toisensa sattumalta pienessä kahvilassa, kun he molemmat
-                                    tilasivat vahvaa kahvia ja istuivat vierekkäin ikkunapöydässä. Aluksi heidän
-                                    keskustelunsa oli kevyttä, mutta pian he huomasivat jakavansa samanlaisen
-                                    huumorintajun ja elämänarvot.</p>
+                                <h1>Jimi Saranen</h1>
+                                <p>Tässä on Jimi. Hän kuuluu Joensuun Hirvenpyytäjien perustajajäseniin.
+                                    Konaukko henkeen ja vereen sekä kova poika paukuttelemaan sitä soitinta mitä ne
+                                    rumpalit soittaa.
+                                    Esittää opiskelevansa ja pitää ainakin omia juttujaan hauskoina. Kuulemma omistaa
+                                    valtavan pesäpallomailan,
+                                    mutta todisteita tästä ei ole. Tykkää videopelaamisesta, vaeltamisesta,
+                                    musisoinnista sekä pään aukomisesta.</p>
+                            </div>
+                            <div class="col-lg-12">
+                                <img src={hirvenpyytaja} alt={"kuva"} width={400} height={400}/>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="post">
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-12">
                             <div id="Kuka">
-                                <p>Jimi, taiteellinen sielu, oli rumpali paikallisessa bändissä. Hänen silmänsä
-                                    kirkastuivat, kun hän puhui musiikistaan ja siitä, miten se toi iloa hänen
-                                    elämäänsä. Leevi, kirjastonhoitaja, oli hiljainen ja varattu, mutta hänen silmissään
-                                    oli syvä viisaus ja uteliaisuus maailmaa kohtaan.</p>
+                                <h1>Leevi-Kalle Mäkinen</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta... Niin ja tosiaan asettelut pitäis varmaan
+                                    korjata!!!
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="post">
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-12">
                             <div id="Kuka">
-                                <p>Pian he alkoivat nähdä toisiaan useammin, ja jokainen kohtaaminen syvensi heidän yhteyttään. Jimi soitti Leeville kauniita säveliä, ja Leevi jakoi hänen kanssaan lempikirjojaan. He kävelivät yhdessä puistoissa ja istuivat rannalla katsellen auringonlaskuja.</p>
+                                <h1>Topi Purtola</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="post">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div id="Kuka">
+                                <h1>Lassi Järvelä</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="post">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div id="Kuka">
+                                <h1>Rytkön Ville</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="post">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div id="Kuka">
+                                <h1>Jykä</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="post">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div id="Kuka">
+                                <h1>Taneli</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="post">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div id="Kuka">
+                                <h1>Vanhus</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta...
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="post">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div id="Kuka">
+                                <h1>Ietu</h1>
+                                <p>Tänne tulee sit sitä siistiä esittelyä jossain vaiheessa, kunhan ehtii, jaksaa ja
+                                    kiinnostaa. Jotain jaarittelua siitä, että
+                                    kuka oot, mitä teet... en tiiä jukolauta...
+                                </p>
                             </div>
                         </div>
                     </div>
